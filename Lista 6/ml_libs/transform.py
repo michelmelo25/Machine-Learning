68cef1debaac64d1a6c06c3a5dbb7343e374441c
Michel Melo <michelmelosj25@hotmail.com>
2019-04-14 14:08:17 -0300

    inicio lista 6, descricoes do dataset e criacao da classe nomalize

---
+++ Lista 6/ml_libs/transform.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+class normalize:
+    
+    vals = {}
+    
+    def fit(X):
+        vals.clear()
+        n_cols = X.shape[1]
+        for i in range(n_cols):
+            vals.update({i: {'min': np.min(X[:,i]),'max': np.max(X[:,i])}})
+        return vals
+            
+    def transform(X):
+        n_cols = X.shape[1]
+        for i in range(n_cols):
+            X[:,i] = (X[:,i] - vals[i]['min'])/(vals[i]['max'] - vals[i]['min'])
+        return X
+            
