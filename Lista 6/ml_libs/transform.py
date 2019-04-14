e36cda8b08f3390920af6bd6a6d6931ce2158279
Michel Melo <michelmelosj25@hotmail.com>
2019-04-14 19:54:06 -0300

    algum avanco

--- Lista 6/ml_libs/transform.py
+++ Lista 6/ml_libs/transform.py
@@ -16,4 +16,20 @@ class normalize:
         for i in range(n_cols):
             X[:,i] = (X[:,i] - vals[i]['min'])/(vals[i]['max'] - vals[i]['min'])
         return X
-            
+
+class standardize :
+    
+    vals = {}
+    
+    def fit(X):
+        vals.clear()
+        n_cols= X.shape[1]
+        for i in range(n_cols):
+            vals.update({i: {'mean': np.mean(X[:,i]),'std': np.std(X[:,i])}})
+        return vals
+    
+    def transform(X):
+        n_cols = X.shape[1]
+        for i in range(n_cols):
+            X[:,i] = (X[:,i] - vals[i]['mean'])/(vals[i]['std'])
+        return X
