cde5935d59976be46329bbd249ed17b06188e966
Michel Melo <michelmelosj25@hotmail.com>
2019-05-06 19:18:42 -0300

    Lista8 terminada

--- Lista 8/ml_libs/metricas.py
+++ Lista 8/ml_libs/metricas.py
@@ -9,27 +9,22 @@ class metricas:
     
     def precision(_self,y_test,pred):
         mc = confusion_matrix(y_test, pred)
-        dfmc = pd.DataFrame(mc)
-        TP = mc.diagonal()
-        FP = []
-        for i in range(dfmc.shape[0]):
-            FP.append(dfmc[i].sum() - mc.diagonal()[i])
-        FP = np.array(FP)
-        return TP.sum()/(TP.sum()+FP.sum())
+        pond = np.unique(y_test, return_counts=True)[1]
+        pred = []
+        for i in range(mc.shape[0]):
+                pred.append(pond[i] * mc.diagonal()[i]/mc[:,i].sum())
+        return np.sum(pred)/pond.sum()
     
     def recall(_self,y_test,pred):
         mc = confusion_matrix(y_test, pred)
-        dfmc = pd.DataFrame(mc)
-        TP = mc.diagonal()
-        FN = []
-        for i in range(dfmc.shape[0]):
-            FN.append(dfmc.values[i].sum() - mc.diagonal()[i])
-        FN = np.array(FN)
-        return TP.sum()/(TP.sum()+FN.sum())
+        pond = np.unique(y_test, return_counts=True)[1]
+        pred = []
+        for i in range(mc.shape[0]):
+            pred.append(pond[i] * mc.diagonal()[i]/mc[i].sum())
+        return np.sum(pred)/pond.sum()
     
     def f1_measure(_self,y_test,pred):
         presc = _self.precision(y_test,pred)
         reca = _self.recall(y_test,pred)
-        
         return 2*(presc*reca)/(presc+reca)
         
